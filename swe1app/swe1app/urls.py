e7ff0a6964eee32355d88aa77eaac23082a28472
Danyal <da2853@nyu.edu>
2024-02-19 20:21:40 -0500

    Added Polls App

--- swe1app/swe1app/urls.py
+++ swe1app/swe1app/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from swe1app import views
+import polls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    
+    path('polls/', include('polls.urls'))
 ]
